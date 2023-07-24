165d5f10fa6d09d99d99434e75f2a4fa6f069fe4
Kevin Stewart <kevin.g.stewart@gmail.com>
2023-07-24 06:40:49 -0400

    Create sslo-create-ai-category.sh

---
+++ sslo-generative-ai-categories/sslo-create-ai-category.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+## Set category names
+AI_CHAT_CATEGORY="SSLO_GENERATIVE_AI_CHAT"
+
+## Set category URLs
+AI_CHAT_URL="https://raw.githubusercontent.com/kevingstewart/sslo-generative-ai-categories/main/ai-category-chat"
+
+## Fetch remote URL category
+curl -sO ${AI_CHAT_URL}
+
+## Get current datetime
+TIMESTAMP=$(date +"%Y%m%d")
+
+## Loop through URL list to build category data
+for url in $(cat ai-category-chat)
+do
+    ## If url contains "*", set as a glob-match URL, otherwise exact-match
+    if [[ $url =~ "*" ]]
+    then
+        url=$(echo $url | sed -E 's/\*/\\*/')
+        str_urls="${str_urls} urls add { \"https://${url}/\" { type glob-match }} urls add { \"http://${url}/\" { type glob-match }}"
+    else
+        str_urls="${str_urls} urls add { \"https://${url}/\" { type exact-match }} urls add { \"http://${url}/\" { type exact-match }}"
+    fi
+done
+
+## Test for existing of custom URL category
+exists=true && [[ "$(tmsh list /sys url-db url-category ${AI_CHAT_CATEGORY} 2>&1)" =~ "was not found" ]] && config=false
+if ($config)
+then
+    ## Category exists - overwrite
+    tmsh -a modify /sys url-db url-category ${AI_CHAT_CATEGORY} display-name "${AI_CHAT_CATEGORY}" urls replace-all-with { https://${TIMESTAMP}/ { type exact-match } } default-action allow 2>&1
+else
+    ## Category doesn't exist - create
+    tmsh -a create /sys url-db url-category ${AI_CHAT_CATEGORY} display-name "${AI_CHAT_CATEGORY}" urls replace-all-with { https://${TIMESTAMP}/ { type exact-match } } default-action allow 2>&1    
+fi
+
+## Populate category with URLs
+tmsh -a modify /sys url-db url-category ${AI_CHAT_CATEGORY} ${str_urls} 2>&1
