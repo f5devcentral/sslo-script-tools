0ff32a637fae1677e2eb161be645a03a05ab5758
Kevin Stewart <kevin.g.stewart@gmail.com>
2024-07-05 15:06:23 -0400

    Update in-t-rule-static.tcl

--- sni-switching/in-t-rule-static.tcl
+++ sni-switching/in-t-rule-static.tcl
@@ -14,21 +14,38 @@
 ##      - Edit this iRule to select the correct client SSL profile based on the SNI. Deploy and test.
 
 when CLIENT_ACCEPTED priority 250 {
-    TCP::collect
+	SSL::disable
+	TCP::collect
 }
 when CLIENT_DATA priority 250 {
+	binary scan [TCP::payload] cSS tls_xacttype tls_version tls_recordlen
+	set tls_length [expr $tls_recordlen+5]
+	if { ([info exists tls_length]) } {
+		if { [TCP::payload length] < $tls_length } {
+			TCP::collect
+			return
+		}
+    }
     ## call the external procedure
-    set sni [call library-rule::getSNI [TCP::payload]]    
+    set sni [call library-rule::getSNI [TCP::payload]]
+    ##log local0. "select sni is $sni"
 
-    switch [string tolower ${sni}] {
-        "foo.f5labs.local" {
-            set cmd1 "SSL::profile /Common/test1-clientssl" ; eval $cmd1
-        }
-        "bar.f5labs.local" {
-            set cmd2 "SSL::profile /Common/test2-clientssl" ; eval $cmd2
-        }
-        "blah.f5labs.local" {
-            set cmd3 "SSL::profile /Common/test3-clientssl" ; eval $cmd3
+    if { ${sni} eq "null" } {
+            set cmd "SSL::disable" ; eval $cmd
+			set cmd "SSL::disable serverside" ; eval $cmd
+    } else {
+       ## lookup SSL profile in data group
+       switch [string tolower ${sni}] {
+            "foo.f5labs.local" {
+                set cmd1 "SSL::profile /Common/test1-clientssl" ; eval $cmd1
+            }
+            "bar.f5labs.local" {
+                set cmd2 "SSL::profile /Common/test2-clientssl" ; eval $cmd2
+            }
+            "blah.f5labs.local" {
+                set cmd3 "SSL::profile /Common/test3-clientssl" ; eval $cmd3
+            }
         }
     }
+    TCP::release
 }
