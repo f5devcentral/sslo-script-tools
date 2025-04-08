153fd7a4f73db3131ed74063c4cb55b57b27b805
Kevin Stewart <kevin.g.stewart@gmail.com>
2025-04-08 11:36:18 -0400

    Update sslo-create-ai-category.sh

--- sslo-generative-ai-categories/sslo-create-ai-category.sh
+++ sslo-generative-ai-categories/sslo-create-ai-category.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 ## Set category names
-AI_CHAT_CATEGORY="SSLO_GENERATIVE_AI_CHAT"
+AI_CATEGORY="SSLO_AI_TOOLS"
 
 ## Set category URLs
-AI_CHAT_URL="https://raw.githubusercontent.com/f5devcentral/sslo-script-tools/main/sslo-generative-ai-categories/ai-tools-urls"
+AI_URLS="https://raw.githubusercontent.com/f5devcentral/sslo-script-tools/main/sslo-generative-ai-categories/ai-tools-urls"
 
 ## Fetch remote URL category
-curl -sO ${AI_CHAT_URL}
+curl -sO ${AI_URLS}
 
 ## Get current datetime
 TIMESTAMP=$(date +"%Y%m%d")
 
 ## Loop through URL list to build category data
-for url in $(cat ai-category-chat)
+for url in $(cat ai-tools-urls)
 do
     ## If url contains "*", set as a glob-match URL, otherwise exact-match
     if [[ $url =~ "*" ]]
@@ -26,18 +26,18 @@ do
 done
 
 ## Test for existing custom URL category
-exists=true && [[ "$(tmsh list /sys url-db url-category ${AI_CHAT_CATEGORY} 2>&1)" =~ "was not found" ]] && config=false
+exists=true && [[ "$(tmsh list /sys url-db url-category ${AI_CATEGORY} 2>&1)" =~ "was not found" ]] && config=false
 if ($config)
 then
     ## Category exists - overwrite
-    tmsh -a modify /sys url-db url-category ${AI_CHAT_CATEGORY} display-name "${AI_CHAT_CATEGORY}" urls replace-all-with { https://${TIMESTAMP}/ { type exact-match } } default-action allow 2>&1
+    tmsh -a modify /sys url-db url-category ${AI_CATEGORY} display-name "${AI_CATEGORY}" urls replace-all-with { https://${TIMESTAMP}/ { type exact-match } } default-action allow 2>&1
 else
     ## Category doesn't exist - create
-    tmsh -a create /sys url-db url-category ${AI_CHAT_CATEGORY} display-name "${AI_CHAT_CATEGORY}" urls replace-all-with { https://${TIMESTAMP}/ { type exact-match } } default-action allow 2>&1    
+    tmsh -a create /sys url-db url-category ${AI_CATEGORY} display-name "${AI_CATEGORY}" urls replace-all-with { https://${TIMESTAMP}/ { type exact-match } } default-action allow 2>&1    
 fi
 
 ## Populate category with URLs
-tmsh -a modify /sys url-db url-category ${AI_CHAT_CATEGORY} ${str_urls} 2>&1
+tmsh -a modify /sys url-db url-category ${AI_CATEGORY} ${str_urls} 2>&1
 
 ## Clean up temp files
-rm -f ai-category-chat
+rm -f ai-tools-urls
